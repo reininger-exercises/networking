e8b043bd39559667c033c9b625a02baaf61bbe6a
Reid Reininger <reidreininger@outlook.com>
2022-07-20 23:22:03 -0700

    Implement client.py

--- socket_intro/client.py
+++ socket_intro/client.py
@@ -1,6 +1,14 @@
 # client program
+import socket
 import sys
+
+ADDRESS = ('localhost', 5000)
+BUFFSIZ = 4096
 
 if __name__ == '__main__':
 	text = input()
-
+	with  socket.create_connection(ADDRESS) as s:
+		s.sendall(text.encode())
+		data = s.recv(BUFFSIZ).decode()
+	print(data)
+		
