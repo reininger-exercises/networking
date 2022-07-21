343f86872e066cdfe163b824ea489859678509c7
Reid Reininger <reidreininger@outlook.com>
2022-07-21 16:50:35 -0700

    Add SIGINT signal handler

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,12 +1,33 @@
 # web server
 from socket import *
+import signal
+import sys
 
 class WebServer:
     def __init__(self, port=12000):
         self._port = port
+        self._bufLen = 2048
+        self._registerSignalHandlers()
+
+    @property
+    def bufLen(self):
+        # Length of max segment size received from client at a time in bytes
+        return self._bufLen
+
+    @bufLen.setter
+    def bufLen(self, newBufLen):
+        self._bufLen = max(0, newBufLen)
+
+    def _registerSignalHandlers(self):
+        signal.signal(signal.SIGINT, self._signal_handler)
+
+    def _signal_handler(slef, sig, frame):
+        print('Shutting down...')
+        sys.exit(0)
 
     def start(self):
         with self._createServerSocket() as serverSocket:
+            print('Ready to accept connections')
             serverSocket.listen(1)
             while True:
                 self._handleConnection(*serverSocket.accept())
@@ -18,4 +39,5 @@ class WebServer:
         return serverSocket
 
     def _handleConnection(self, connectionSocket, address):
-        print('Server connected to: ', address)
+        with connectionSocket:
+            print('Server connected to: ', address)
