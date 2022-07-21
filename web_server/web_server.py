f30c98290ebaa573e894b7740c57da2e89a520ba
Reid Reininger <reidreininger@outlook.com>
2022-07-21 16:05:28 -0700

    Implement WebServer._createServerSocket

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -3,4 +3,10 @@ from socket import *
 
 class WebServer:
     def __init__(self, port=12000):
-        self._portNumber = port
+        self._port = port
+
+    def _createServerSocket(self):
+        # Creates and binds a TCP server socket for initiating connections.
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind(('', self._portNumber))
+        return serverSocket
