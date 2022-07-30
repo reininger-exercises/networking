8f8c29c3118de4828f5ea3ab0827f7b58be9e902
Reid Reininger <reidreininger@outlook.com>
2022-07-30 16:56:45 -0700

    Add timeout and print server response

--- udp_pinger/UDPPingerClient.py
+++ udp_pinger/UDPPingerClient.py
@@ -5,8 +5,15 @@ from socket import *
 serverName = 'localhost'
 serverPort = 12000
 pingCount = 10
+socketTimeoutSeconds = 1
 
 with socket(AF_INET, SOCK_DGRAM) as clientSocket:
+    clientSocket.settimeout(socketTimeoutSeconds)
     for ping in range(1, pingCount + 1):
-        pingMessage = f'Ping {pingCount}'
+        pingMessage = f'Ping {ping}'
         clientSocket.sendto(pingMessage.encode(), (serverName, serverPort))
+        try:
+            message, address = clientSocket.recvfrom(2048)
+            print('server responded with: ', message.decode())
+        except:
+            print('Ping timed out')
