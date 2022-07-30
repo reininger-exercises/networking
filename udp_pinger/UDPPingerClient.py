7c57f2392c26c862fe113563804076da71cf4549
Reid Reininger <reidreininger@outlook.com>
2022-07-30 15:43:07 -0700

    Send message to UDPPingServer

--- udp_pinger/UDPPingerClient.py
+++ udp_pinger/UDPPingerClient.py
@@ -1,2 +1,12 @@
 # UDPPingerClient.py
 # Sends pings to UDPPingServer and calculates RTT
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+pingCount = 10
+
+with socket(AF_INET, SOCK_DGRAM) as clientSocket:
+    for ping in range(1, pingCount + 1):
+        pingMessage = f'Ping {pingCount}'
+        clientSocket.sendto(pingMessage.encode(), (serverName, serverPort))
