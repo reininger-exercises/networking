eb75a7664237f9cffcffe14fc9c447d3611201f1
Reid Reininger <32531948+reininger@users.noreply.github.com>
2022-07-31 14:08:27 -0700

    Add SMTP client (#30)
    Closes # 28
    
    * Add smtp_client.py skeleton
    
    * Add outlook push mailserver
    
    * Create clientSocket & connect to mail server
    
    * Send MAIL FROM johnDoe
    
    * Authenticate with mail server
    
    * Add MAIL FROM command
    
    * Add RCPT TO command
    
    * Send message
    
    * Send QUIT command
    
    Co-authored-by: Reid Reininger <reidreininger@outlook.com>
    
    Close socket on terminate

---
+++ smtp/smtp_client.py
@@ -0,0 +1,135 @@
+# Skeleton Python Code for the Mail Client
+from base64 import *
+from email import message
+from socket import *
+from ssl import *
+from textwrap import wrap
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+
+# load crednetials
+with open('credentials.txt', 'r') as credentialFile:
+	lines = credentialFile.readlines()
+	lines = [line.strip() for line in lines]
+	username, password = lines[:2]
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = ('smtp.office365.com', 587)
+
+# Create socket called clientSocket and establish a TCP connection with
+# mailserver
+try:
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+except:
+	exit(1)
+
+def terminate():
+	clientSocket and clientSocket.close()
+	exit(1)
+
+clientSocket.connect(mailserver)
+
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+	print('220 reply not received from server.')
+	terminate()
+
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+	terminate()
+
+# Send Starttls command
+startTlsCommand = 'STARTTLS\r\n'
+clientSocket.send(startTlsCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+	print('220 reply not received from server.')
+	terminate()
+
+clientSocket = wrap_socket(clientSocket, ssl_version=PROTOCOL_SSLv23)
+
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+	terminate()
+
+# authenticate
+authLoginCommand = 'AUTH LOGIN\r\n'
+clientSocket.send(authLoginCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '334':
+	print('334 reply not received from server.')
+	terminate()
+
+# send credentials
+clientSocket.send(b64encode(username.encode())+b'\r\n')
+recv = clientSocket.recv(1024).decode()
+print(recv)
+clientSocket.send(b64encode(password.encode())+b'\r\n')
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '235':
+	print('235 reply not received from server.')
+	terminate()
+
+# Send MAIL FROM command and print server response.
+mailFromCommand = f'MAIL FROM: <{username}>\r\n'
+clientSocket.send(mailFromCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+	terminate()
+
+# Send RCPT TO command and print server response.
+rcptToCommand = f'RCPT TO: <{username}>\r\n'
+clientSocket.send(rcptToCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+	terminate()
+
+# Send DATA command and print server response.
+dataCommand = f'DATA\r\n'
+clientSocket.send(dataCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '354':
+	print('354 reply not received from server.')
+	terminate()
+
+# Send message data.
+clientSocket.send(msg.encode())
+
+# Message ends with a single period.
+clientSocket.send(endmsg.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+	terminate()
+
+# Send QUIT command and get server response.
+quitCommand = f'QUIT\r\n'
+clientSocket.send(quitCommand.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '221':
+	print('221 reply not received from server.')
+	terminate()
+
+terminate()
